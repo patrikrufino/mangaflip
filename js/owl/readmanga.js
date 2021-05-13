09e2152fb6443d75b089d05078c42684cf5a6185
Patrik Rufino <patrik.rufino@gmail.com>
2021-05-13 09:58:55 -0300

    Add pages, nanatsu

--- js/owl/readmanga.js
+++ js/owl/readmanga.js
@@ -1,17 +1,16 @@
 $('.owl-carousel').owlCarousel({
     loop:true,
-    margin:12,
-    nav:false,
-    animateOut: 'fadeOut',
+    margin:15,
+    nav:true,
     responsive:{
         0:{
             items:1
         },
         600:{
-            items:3
+            items:1
         },
         1000:{
-            items:5
+            items:1
         }
     }
 })
